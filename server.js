c8d43b6377487cb0a19e775dc183a72b292f39e0
Cláudio Yuri <claudio.y07@gmail.com>
2019-12-12 14:22:52 -0300

    Lidando com erros

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const weather = require('openweather-apis');
+const SpotifyWebApi = require('spotify-web-api-node');
 const axios = require('axios');
 const querystring = require('querystring');
 const express = require('express');
 const app = express();
 
 weather.setLang('en');
-
-weather.setAPPID('b77e07f479efe92156376a8b07640ced');
 
 app.get('/', async (req, resp) =>{
 
@@ -20,13 +19,27 @@ app.get('/', async (req, resp) =>{
         search = querystring.stringify({ lat, lon });
     }
 
-    let url = `http://api.openweathermap.org/data/2.5/weather?${search}&units=metric&appid=b77e07f479efe92156376a8b07640ced`;
+    let urlOpenWeather = `http://api.openweathermap.org/data/2.5/weather?${search}&units=metric&appid=b77e07f479efe92156376a8b07640ced`;
 
-    await axios.get(url)
-        .then((res) => {
-          resp.send(res.data);
-        })
-        .catch((err) => { resp.status(err.response.status).send({ message: err.response.statusText }) })
+    try {
+
+        const responseTemp =  await axios.get(urlOpenWeather);
+
+        const { temp } = responseTemp.data.main;
+
+        spotifyApi.
+
+        resp.json({ temp });
+        
+    } catch (error) {
+
+        const { response } = error;
+
+        const errorStatus = !response ? 500 : response.status;
+        const errorMessage = !response ? "Server error" : response.statusText;
+        
+        resp.sendStatus(errorStatus).send({ message: errorMessage }); 
+    }
 
     resp.json();
 });
